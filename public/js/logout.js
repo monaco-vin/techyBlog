b5be9be12c160f35538f246d0e0b2549465bb443
Rocket Appliances <monaco.vin@gmail.com>
2022-08-24 12:13:22 -0500

    ADD public js login, comment, post, edit post, logout

---
+++ public/js/logout.js
@@ -0,0 +1,17 @@
+const logoutBtn = document.getElementById("logout");
+
+const logoutSession = async (event) => {
+  event.preventDefault();
+
+  const response = await fetch("/api/logout", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (response.ok) {
+    document.location.replace("/");
+  } else {
+    alert(response.statusText);
+  }
+};
+logoutBtn.addEventListener("click", logoutSession);
